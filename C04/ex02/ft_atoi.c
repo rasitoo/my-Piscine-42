070e09130c0b25b53c2c0ff5ef32275591a6131e
rasitoo <05rotapi@gmail.com>
2025-07-21 11:44:17 +0200

    add

--- C04/ex02/ft_atoi.c
+++ C04/ex02/ft_atoi.c
@@ -1,16 +1,37 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rtapiado <rtapiado@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/07/20 21:10:33 by rtapiado          #+#    #+#             */
-/*   Updated: 2025/07/20 21:10:33 by rtapiado         ###   ########.fr       */
+/*   Created: 2025/07/21 10:39:50 by rtapiado          #+#    #+#             */
+/*   Updated: 2025/07/21 10:39:50 by rtapiado         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_strlen(char *str)
+#include <stdio.h>
+
+int	ft_atoi(char *str)
 {
-sfd
+	int		nb_lgth;
+	// int		dec_pos;
+	// int		i;
+	int		res;
+	// char	c;
+
+	nb_lgth = 0;
+	res = 0;
+	while (str[nb_lgth] != '\0' && str[nb_lgth] > '0' && str[nb_lgth] > '9')
+		nb_lgth++;
+	while (nb_lgth)
+	{
+		res += (str[nb_lgth--]);
+	}
+	return (res);
+}
+
+int	main(void)
+{
+	printf("%d",ft_atoi("214324"));
 }
